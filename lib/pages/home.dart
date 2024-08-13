c4d39c52b8d81fde65814905150d2cfb37fe6153
AbishekAB21 <abishekab1805@outlook.com>
2024-08-13 12:57:50 +0530

    commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     // background should be a gradient preferebly black
+     // Use some lottie animations or something 
+     // Display my name and role on the left center and my photo on the right
+     // then move on to the skills section 
+     // then display your projects
+     // educational background ? idk maybe 
+     // contact me section with "name" "email" and "Message"
+     // social media buttons LinkedIn, Insta, X, Github 
+     // should be responsive on phone and web
+     // Use some good fonts not the default one 
+     // Refference : Shohruh Ak on YT 
+      body: Center(
+        child: Text("Body"),
+      ),
+    );
+  }
+}
