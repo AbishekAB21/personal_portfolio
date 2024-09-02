ae08c2a6dc09bb43b7f502993b30a7d9a93c7da7
AbishekAB21 <abishekab1805@outlook.com>
2024-09-03 00:49:50 +0530

    commit

---
+++ lib/widgets/add_projects_container.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:personal_portfolio/provider/admin_screen_provider.dart';
+import 'package:personal_portfolio/utils/fontstyles.dart';
+import 'package:personal_portfolio/widgets/image_circle.dart';
+import 'package:personal_portfolio/widgets/reusable_button.dart';
+import 'package:personal_portfolio/widgets/reusable_textformfield.dart';
+import 'package:provider/provider.dart';
+
+class AddProjectContainer extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final TextEditingController nameController;
+  final TextEditingController descriptionController;
+  final TextEditingController githubController;
+
+  const AddProjectContainer({
+    required this.formKey,
+    required this.nameController,
+    required this.descriptionController,
+    required this.githubController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AdminProvider>(
+      builder: (context, provider, child) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          margin: EdgeInsets.symmetric(horizontal: 200),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(20)),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Add a New Project",
+                  style: Fonstyles.HeadingTextStyle2(context),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ImageCircle(imageBytes: provider.imageBytes, ontap: provider.pickImage,),
+                SizedBox(
+                  height: 30,
+                ),
+                ReusableTextFormField(
+                  cntrlr: nameController,
+                  hinttext: "Project Name",
+                  icon: Icon(Icons.assessment_rounded),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ReusableTextFormField(
+                  cntrlr: descriptionController,
+                  hinttext: "Project Description",
+                  icon: Icon(Icons.description_rounded),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ReusableTextFormField(
+                  cntrlr: githubController,
+                  hinttext: "Github URL",
+                  icon: Icon(Icons.link_rounded),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ReusableButton(
+                  buttontext: "Add To Database",
+                  onTap: () async {
+                    if (formKey.currentState!.validate()) {
+                      print("FORM VALIDATED");
+                      await provider.uploadProject(
+                          projectName: nameController.text,
+                          projectDescription:
+                              descriptionController.text,
+                          githubLink: githubController.text);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              duration: Duration(seconds: 3),
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .tertiary,
+                              content: Text(
+                                "Successfully added the new project",
+                                style: Fonstyles.RegularTextStyle(
+                                    context),
+                              )));
+                      descriptionController.clear();
+                      nameController.clear();
+                      githubController.clear();
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              duration: Duration(seconds: 3),
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .error,
+                              content: Text(
+                                "Error adding the new project",
+                                style: Fonstyles.RegularTextStyle(
+                                    context),
+                              )));
+                    }
+                  },
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/widgets/drawrer_widget.dart
+++ lib/widgets/drawrer_widget.dart
@@ -4,12 +4,18 @@ import 'package:personal_portfolio/utils/fontstyles.dart';
 import 'package:personal_portfolio/widgets/drawrer_buttons.dart';
 
 class MyDrawrer extends StatelessWidget {
-   final Function(GlobalKey, {double offset}) onSectionSelected;
-   final GlobalKey aboutMeKey;
-   final GlobalKey myProjectsKey;
-   final GlobalKey myskillsKey;
-   final GlobalKey contactMeKey;
-   MyDrawrer({super.key, required this.aboutMeKey, required this.myProjectsKey, required this.myskillsKey, required this.contactMeKey, required this.onSectionSelected});
+  final Function(GlobalKey, {double offset}) onSectionSelected;
+  final GlobalKey aboutMeKey;
+  final GlobalKey myProjectsKey;
+  final GlobalKey myskillsKey;
+  final GlobalKey contactMeKey;
+  MyDrawrer(
+      {super.key,
+      required this.aboutMeKey,
+      required this.myProjectsKey,
+      required this.myskillsKey,
+      required this.contactMeKey,
+      required this.onSectionSelected});
 
   @override
   Widget build(BuildContext context) {
@@ -38,32 +44,11 @@ class MyDrawrer extends StatelessWidget {
               height: 30,
             ),
 
-// Admin Mode
-            GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AdminScreen(),
-                      ));
-                },
-                child: DrawrerButton(
-                    title: "Admin Mode",
-                    icon: Icon(
-                      Icons.lock,
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                    ))),
-            SizedBox(
-              height: 20,
-            ),
-
 // About Me
             GestureDetector(
               onTap: () {
                 Navigator.pop(context);
-                onSectionSelected(aboutMeKey, offset : 100.0);
-               
+                onSectionSelected(aboutMeKey, offset: 100.0);
               },
               child: DrawrerButton(
                   title: "About Me",
@@ -80,7 +65,6 @@ class MyDrawrer extends StatelessWidget {
             GestureDetector(
               onTap: () {
                 Navigator.pop(context);
-                
               },
               child: DrawrerButton(
                   title: "My Skills",
@@ -111,12 +95,40 @@ class MyDrawrer extends StatelessWidget {
             ),
 
 // Contact Me
-            DrawrerButton(
-                title: "Contact Me",
-                icon: Icon(
-                  Icons.mail_rounded,
-                  color: Theme.of(context).colorScheme.inversePrimary,
-                )),
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: DrawrerButton(
+                  title: "Contact Me",
+                  icon: Icon(
+                    Icons.mail_rounded,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+// Admin Mode
+            GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AdminScreen(),
+                      ));
+                },
+                child: DrawrerButton(
+                    title: "Admin Mode",
+                    icon: Icon(
+                      Icons.lock,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ))),
+            SizedBox(
+              height: 20,
+            ),
           ],
         ));
   }
